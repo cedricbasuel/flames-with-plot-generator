a3da159ede56b268359bbfa43d974daa85365910
cedric <cedric.basuel@gmail.com>
2021-01-14 14:41:03 +0800

    added config in  training code

--- train-text-gen-gpt2.py
+++ train-text-gen-gpt2.py
@@ -14,85 +14,96 @@ from transformers import (
     set_seed,
     AutoModelWithLMHead,
 )
-import pandas
+import logging
 
-
-
-# tokenizer
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
-print('loading tokenizer...')
-# tokenizer = GPT2Tokenizer.from_pretrained(
-#     'gpt2', 
-#     bos_token='<BOS>', 
-#     eos_token='<EOS>', 
-#     pad_token='<|pad|>'
-#     )
-
-
-# model
-print('loading pretrained GPT2 model...')
-# model = GPT2LMHeadModel.from_pretrained('gpt2')
-model = AutoModelWithLMHead.from_pretrained('gpt2')
-
-# add special token dictio
-special_tokens_dict = {
-    'bos_token': '<BOS>',
-    'eos_token': '<EOS>',
-}
-
-# model resize token emb??? AHHH kasi nga nag dagdag ng special tokens so mag-aadjust din ung length ng embeddings
-tokenizer.add_special_tokens(special_tokens_dict)
-model.resize_token_embeddings(len(tokenizer))
-
-
-# load data
-# data is .txt file with one entry per line
-print('loading data...')
-train_data = TextDataset(
-    tokenizer=tokenizer, 
-    file_path='/home/cedric/Desktop/cv-tf2/plots_train_gpt2.txt',
-    block_size=50
+logging.basicConfig(
+    format='[TEXT-GENETRATION:TRAIN] %(asctime)s %(message)s', 
+    datefmt='%m/%d/%Y %I:%M:%S %p', 
+    level=logging.DEBUG
     )
 
-test_data = TextDataset(
-    tokenizer=tokenizer,
-    file_path='/home/cedric/Desktop/cv-tf2/plots_test_gpt2.txt',
-    block_size=50
-)
+if __name__ == '__main__':
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer,
-    mlm=False
-)
+    CONFIG_FILE = sys.argv[1]
 
-# train model
-print('training...')
-training_args = TrainingArguments(
-    output_dir="/home/cedric/Desktop/cv-tf2/text-generation",
-    overwrite_output_dir=True,
-    do_train=True,
-    do_eval=True,
-    # evaluate_during_training=False,
-    # logging_steps=500,
-    # per_device_train_batch_size=4,
-    num_train_epochs=3,
-    # save_total_limit=1,
-    save_steps=10000,
-    prediction_loss_only=True,
-)
+    with open(CONFIG_FILE) as cfg:
+        config = yaml.safe_load(cfg)
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_data,
-    eval_dataset=test_data,
-    data_collator=data_collator
-)
+    # tokenizer
+    logging.info('Loading tokenizer...')
+    tokenizer = AutoTokenizer.from_pretrained(config['gpt_train']['model_name'])
+    # tokenizer = GPT2Tokenizer.from_pretrained(
+    #     'gpt2', 
+    #     bos_token='<BOS>', 
+    #     eos_token='<EOS>', 
+    #     pad_token='<|pad|>'
+    #     )
 
-trainer.train()
 
-# save model
-print('saving model...')
-trainer.save_model('/home/cedric/Desktop/cv-tf2/text-generation')
-tokenizer.save_pretrained('/home/cedric/Desktop/cv-tf2/text-generation')
+    # model
+    logging.info('Loading pretrained weights GPT2 model...')
+    # model = GPT2LMHeadModel.from_pretrained('gpt2')
+    model = AutoModelWithLMHead.from_pretrained(config['gpt_train']['model_name'])
+
+    # add special token dictio
+    special_tokens_dict = {
+        'bos_token': '<BOS>',
+        'eos_token': '<EOS>',
+    }
+
+    # model resize token emb??? AHHH kasi nga nag dagdag ng special tokens so mag-aadjust din ung length ng embeddings
+    tokenizer.add_special_tokens(special_tokens_dict)
+    model.resize_token_embeddings(len(tokenizer))
+
+
+    # load data
+    # data is .txt file with one entry per line
+    logging.info('Loading data...')
+    train_data = TextDataset(
+        tokenizer=tokenizer, 
+        file_path=config['gpt_train']['train_data'],
+        block_size=config['gpt_train']['block_size']
+        )
+
+    test_data = TextDataset(
+        tokenizer=tokenizer,
+        file_path=config['gpt_train']['eval_data'],
+        block_size=config['gpt_train']['block_size']
+    )
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=False
+    )
+
+    # train model
+    logging.info('Training model...')
+    training_args = TrainingArguments(
+        output_dir=config['gpt_train']['dir'],
+        overwrite_output_dir=True,
+        do_train=True,
+        do_eval=True,
+        # evaluate_during_training=False,
+        # logging_steps=500,
+        # per_device_train_batch_size=4,
+        num_train_epochs=config['gpt_train']['epochs'],
+        # save_total_limit=1,
+        save_steps=config['gpt_train']['save_steps'],
+        prediction_loss_only=True,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=test_data,
+        data_collator=data_collator
+    )
+
+    trainer.train()
+
+    # save model
+    logging.info('Saving model...')
+    trainer.save_model(config['gpt_train']['dir'])
+    tokenizer.save_pretrained(config['gpt_train']['dir'])
 
