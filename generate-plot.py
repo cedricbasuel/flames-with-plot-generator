28005d8e36c6c1f8e6b559d190932202abcfcdb0
cedric <cedric.basuel@gmail.com>
2021-01-14 12:08:29 +0800

    added text generation code given trained  model

--- generate-plot.py
+++ generate-plot.py
@@ -1,5 +1,26 @@
+from transformers import pipeline, TextGenerationPipeline, GPT2LMHeadModel, AutoTokenizer
 
 # load chrained model
+model = GPT2LMHeadModel.from_pretrained('/home/cedric/Downloads/text-generation-1/text-generation')
+
 # load chokenizer
+tokenizer = AutoTokenizer.from_pretrained('/home/cedric/Downloads/text-generation-1/text-generation')
+
 # use transformers's text generation pipeline
-# then input prompt tska story_generator
+story_generator = TextGenerationPipeline(model=model, tokenizer=tokenizer)
+
+# input prompt 
+input_prompt = '<BOS> ' # dagdag pa ung results ng flames
+
+# generate text
+text = story_generator(
+    input_prompt,
+    max_length=100,
+    do_sample=True,
+    temperature=0.9,
+    top_p=0.95,
+    top_k=50,
+    repetition_penalty=1.1,
+    num_return_sequences=5,
+)
+
