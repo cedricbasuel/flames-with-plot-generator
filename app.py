5f9c8db30b19de104e988f109c2b87492a26a69f
cedric <cedric.basuel@gmail.com>
2021-01-23 01:23:22 +0800

    next step: add flask config.py

--- app.py
+++ app.py
@@ -1,55 +1,62 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 import flames
+import flames_with_generate_plot
+import sys, yaml
+
+
+# replace with flask config!!
+CONFIG_FILE = sys.argv[1]
+
+with open(CONFIG_FILE) as cfg:
+    config = yaml.safe_load(cfg)
 
 app = Flask(__name__)
 
-
-prompts = {
-    'Friendship': [', who is friends with ', ', in a friendship with ', ', a good friend of '],
-    'Love': [', who is in love with ', ' who loves ', ', lover of ', ', the partner of '],
-    'Affection': [', who has affection for ', ' who is affectionate with ', ' who has nothing but affection for '],
-    'Marriage': [', who is married to ', ' the partner of '],
-    'Enemy': [', the enemy of ',  ', who hates ', ' the rival of ', ' who is engaged in a rivalry with '],
-    'Sibling': [', the sibling of '],
-}
-
-# load trained model
-model = GPT2LMHeadModel.from_pretrained(config['gpt_generate']['dir'])
-
-# load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(config['gpt_generate']['dir'])
-
-# use transformers's text generation pipeline
-story_generator = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=0)
-
-
+story_generator = flames_with_generate_plot.load_gpt_model(
+        model_path=config['gpt_generate']['dir'], 
+        tokenizer_path=config['gpt_generate']['dir'],
+        device=0
+        )
 
 @app.route('/')
 def home():
-
-
-
-    return render_template('home.html')
+    return render_template('home1.html')
 
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method=='POST':
 
-        name1 = request.form.get()
-        name2 = request.form.get()
+    name1 = request.form['name1']
+    name2 = request.form['name2']
 
-        unique_letters = flames.remove_common_letters(name1, name2)
-        flames_status = flames.get_flames_status(unique_letters)
+    unique_letters = flames.remove_common_letters(name1, name2)
+    flames_status = flames.get_flames_status(unique_letters)
 
+    input_prompts = flames_with_generate_plot.create_input_prompts(
+        name1, 
+        name2, 
+        flames_status, 
+        n_plots=config['gpt_generate']['n_plots']
+        )
 
+    plots = flames_with_generate_plot.generate_plot(
+        story_generator,
+        input_prompts=input_prompts,
+        temperatures=config['gpt_generate']['temperatures'],
+        max_length=config['gpt_generate']['max_length'],
+        do_sample=config['gpt_generate']['do_sample'],
+        top_p=config['gpt_generate']['top_p'],
+        top_k=config['gpt_generate']['top_k'],
+        repetition_penalty=config['gpt_generate']['rep_penalty'],
+        num_return_sequences=config['gpt_generate']['num_return_sequences'],
+        )
 
+    plots_list = [plot['generated_text'].replace('<BOS> ','') for plot in plots]
 
-    return render_template('home.html', stories=stories)
-
+    return render_template('home1.html', stories=plots_list, status=flames_status)
 
 
 
 if __name__ == "__main__" :
-    app.run(debug=False)
+    app.run(debug=True)
